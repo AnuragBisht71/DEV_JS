080e9cbff2d9bdf8d9736326d8e232958fb6599d
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-04 21:46:14 +0530

    folder sorting

---
+++ Module1/Folder_Sorting/activity/util.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "Documents" : ["doc" , "pdf" , "txt"],
+    "Images" : ["img" , "png" , "jpg"],
+    "Audio" : ["mp3"],
+    "Video" : ["mp4" , "mkv"],
+    "Applications" : ["exe"]
+};
