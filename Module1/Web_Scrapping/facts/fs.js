2f84cc457acc68c6b3155ae4a34730d8678b1b32
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-06 19:52:10 +0530

    css selectors

--- Module1/Web_Scrapping/facts/fs.js
+++ Module1/Web_Scrapping/facts/fs.js
@@ -5,5 +5,31 @@ let cheerio = require("cheerio");
 let html = fs.readFileSync("./index.html" , "utf8");
 
 let myDocument = cheerio.load(html);
-let h1 = myDocument("h1").text();
-console.log(h1);
+// let h1 = myDocument("h1").text();
+// console.log(h1);
+
+// Selectors---------->
+
+// You will get all a tags
+// console.log(myDocument("a").text());
+
+// You will get all a tag inside ul
+// console.log(myDocument("ul a").text());
+
+// You will get all a tag inside li
+// console.log(myDocument("ul li a").text());
+
+// You will get only direct child of ul
+// console.log(myDocument("ul>a").text());
+
+
+// Classes and Ids
+// Classes
+console.log(myDocument(".main").text());
+console.log(myDocument(".main.p").text());
+
+// Id
+console.log(myDocument("#main").text());
+
+
+
