16edbaf3a245d1802de99e35b21c758547e21857
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-09 17:23:28 +0530

    json

--- Module1/Web_Scrapping_2/facts/test.js
+++ Module1/Web_Scrapping_2/facts/test.js
@@ -5,7 +5,11 @@ let obj = [{
     Balls : "2"
 }]
 
-let jsonObj = JSON.stringify(obj);
-fs.writeFileSync("./a.json" , jsonObj);
+let jsonObj = JSON.stringify(obj); // TO CONVERT JS DATA TO JSON DATA -------> {JSON.stringify}
+// fs.writeFileSync("./a.json" , jsonObj);
+
+
+let ObjRead = JSON.parse(fs.readFileSync("./a.json")); // TO CONVERT JSON DATA TO JS DATA -------> {JSON.parse}
+console.log(ObjRead);
 
 
