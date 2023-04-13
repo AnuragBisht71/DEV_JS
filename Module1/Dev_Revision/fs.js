57a29b74d3b38370a9448026a08f4a886fe8c18b
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-13 18:54:05 +0530

    Dev Revision

---
+++ Module1/Dev_Revision/fs.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+// FS Function:-
+
+// fs.writeFileSync("path" , "content")
+// fs.readFileSync("path" , "utf8")
+// fs.mkdirSync("path")
+// fs.copyFileSync()
+// fs.unlinkSync()
+// fs.deleteSync()
