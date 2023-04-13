5f834df334235d6a43cad6f58b06c45658c0f28a
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-13 18:34:43 +0530

    git projects

---
+++ Module1/Dev_Revision/GitScapper/getTopicProjects.js
@@ -0,0 +1,32 @@
+const request = require("request");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+
+function getTopicProjects(allTopicsName , allLinks) {
+    request(allLinks , function(err , res , data) {
+        processData(allTopicsName , data);
+    })
+}
+
+function processData(allTopicsName , data) {
+    let myDocument = cheerio.load(data);
+    let allProjectH1Tags = myDocument(".f3");
+    let topicFolderPath = `./Topics/${allTopicsName}`;
+    let projectFile = [];
+
+    for(let i = 0 ; i < 10 ; i++) {
+        let projectH1Tags = allProjectH1Tags[i];
+        let bothATags = myDocument(projectH1Tags).find("a");
+        let projectATag = myDocument(bothATags[1]);
+        let projectName = projectATag.text().split("\n")[1].trim();
+        let projectLink = "https://github.com"+projectATag.attr("href");
+        projectFile.push({projectName , projectLink});    
+    }
+
+    fs.writeFileSync(`${topicFolderPath}/project.json` , JSON.stringify(projectFile));
+}
+
+
+
+module.exports.getTopicProjects = getTopicProjects;
--- Module1/Dev_Revision/GitScapper/homepage.js
+++ Module1/Dev_Revision/GitScapper/homepage.js
@@ -1,12 +1,14 @@
 const link = "https://github.com/topics";
 const request = require("request");
 const cheerio = require("cheerio");
+const fs = require("fs");
 
 request(link , function(err, res, data) {
     processData(data);
 })
 
-let gitHubTopics = [];
+// let gitHubTopics = [];
+const {getTopicProjects} = require("./getTopicProjects");
 
 function processData(data) {
     let myDocument = cheerio.load(data);
@@ -16,8 +18,11 @@ function processData(data) {
         let allTopics = myDocument(allATags[i]).find("a");
         let allLinks = "https://github.com"+allTopics.attr("href");
         let allTopicsName = allTopics.find(".f3.lh-condensed.text-center").text().split("\n")[1].trim();
-        
-        gitHubTopics = [ {TopicName : allTopicsName , Links : allLinks} ];
-        console.log(gitHubTopics);
+        let topicFolderPath = `./Topics/${allTopicsName}`;
+        fs.mkdirSync(topicFolderPath);
+        getTopicProjects(allTopicsName , allLinks);
+        // gitHubTopics = [ {TopicName : allTopicsName , Links : allLinks} ];
+        // console.log(gitHubTopics);
     }
+
 }
