eff723f01dd27a3e930a93b4c40047d6fe09b3ad
AnuragBisht71 <bishtanurag71@gmail.com>
2023-04-20 12:14:41 +0530

    Logged in

--- Module1/T8.Automation/login.js
+++ Module1/T8.Automation/login.js
@@ -1,17 +1,31 @@
 const puppeteer = require("puppeteer");
+const id = "towofit285@gam1fy.com";
+const pw = "123456789";
+let tab;
 
-let browserOpenPromise = puppeteer.launch({headless : false});
-
-console.log(browserOpenPromise);
+let browserOpenPromise = puppeteer.launch({
+    headless : false, 
+    defaultViewport : null, 
+    args : ["--start-maximized"],
+});
 
 browserOpenPromise.then(function(browser) {
     console.log("Browser is opened !!");
     return browser.pages();
 })
 .then(function(pages){
-    let tab = pages[0];
-    return tab.goto("https://google.com");
+    tab = pages[0];
+    return tab.goto("https://www.hackerrank.com/auth/login");
 })
-.then(function(){
-    console.log("On google homepage");
+.then(function() {
+    return tab.type("#input-1" , id);
+})
+.then(function() {
+    return tab.type("#input-2" , pw);
+})
+.then(function() {
+    return tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+})
+.then(function() {
+    console.log("logged in !!");
 })
