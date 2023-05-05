bb073d13ed9c316bba20c884f5885aaebe63c0c2
AnuragBisht71 <bishtanurag71@gmail.com>
2023-05-05 22:26:50 +0530

    added selecterFiltered class

--- Module2/Ticket_Management_App/activity/script.js
+++ Module2/Ticket_Management_App/activity/script.js
@@ -1,9 +1,17 @@
 let allFilter = document.querySelectorAll(".filter");
 let ticketsContainer = document.querySelector(".tickets-container");
+let openModal = document.querySelector(".open-modal");
+let closeModal = document.querySelector(".close-modal");
+
+let ticketModalOpen = false;
+let isTextTyped = false;
 
 for(let i = 0 ; i < allFilter.length ; i++) {
     allFilter[i].addEventListener("click" , selectFilter);
 }
+
+openModal.addEventListener("click", openTicketModal);
+closeModal.addEventListener("click", closeTicketModal);
 
 function selectFilter(e) {
     let filterSelected = e.target.classList[1];
@@ -14,3 +22,52 @@ function selectFilter(e) {
 
     ticketsContainer.classList.add(filterSelected);
 }
+
+function openTicketModal(e) {
+    if(ticketModalOpen) {
+        return;
+    }
+    let ticketModal = document.createElement("div");
+    ticketModal.classList.add("ticket-modal");
+    ticketModal.innerHTML = `<div class="ticket-text" spellcheck = "false" contenteditable = "true">Type here</div>
+    <div class="ticket-filters">
+        <div class="ticket-filter red selected-filter"></div>
+        <div class="ticket-filter blue"></div>
+        <div class="ticket-filter green"></div>
+        <div class="ticket-filter yellow"></div>
+        <div class="ticket-filter black"></div>
+    </div>`;
+
+    document.querySelector("body").append(ticketModal);
+    ticketModalOpen = true;
+
+    let ticketTextDiv = ticketModal.querySelector(".ticket-text");
+    ticketTextDiv.addEventListener("keypress" , handleKeyPress);
+
+    let ticketFilters = ticketModal.querySelectorAll(".ticket-filter");
+    for(let i = 0 ; i < ticketFilters.length ; i++) {
+        ticketFilters[i].addEventListener("click" , function(e) {
+            if(e.target.classList.contains("selected-filter")) {
+                return;
+            }
+            document.querySelector(".selected-filter").classList.remove("selected-filter");
+            e.target.classList.add("selected-filter");
+        })
+    }
+}
+
+function closeTicketModal(e) {
+    if(ticketModalOpen) {
+        document.querySelector(".ticket-modal").remove();
+        ticketModalOpen = false;
+        isTextTyped = false;
+    }
+}
+
+function handleKeyPress(e) {
+    if(!isTextTyped) {
+        isTextTyped = true;
+        e.target.textContent = "";
+    }
+}
+
