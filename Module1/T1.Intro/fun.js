e46bb065818b1437021a61462f5b040916570b56
AnuragBisht71 <bishtanurag71@gmail.com>
2023-07-13 15:31:19 +0530

    callback and high order function

--- Module1/T1.Intro/fun.js
+++ Module1/T1.Intro/fun.js
@@ -1,27 +1,33 @@
-// function fun() {
-//     console.log("hi I am Anurag");
-// }
+function fun() {
+    console.log("hi I am Anurag");
+}
 
 // fun();
 
-// let sayHi;
+let sayHi;
 
-// sayHi = function() {
-//     console.log("sayHi function");
-// }
+sayHi = function() {
+    console.log("sayHi function");
+}
 
 // sayHi();
 
-function callback() {
+
+
+// High Order Function => functions which accepts function as a argument
+// Callback Function => function which are passed as a argument in a function
+
+
+function callback() { // This is a callback function
     console.log("Hi bitches");
     return 5;
 }
 
-function highorderfunction(cb) {
+function highorderfunction(cb) { 
     let value = cb();
     console.log(value);
 }
 
-highorderfunction(callback);
+highorderfunction(callback); // This is a high order function
 
  
