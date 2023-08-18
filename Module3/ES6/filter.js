a480b6a230dfc8387b713dd53d8ef1d299d00ac9
AnuragBisht71 <bishtanurag71@gmail.com>
2023-08-18 14:11:46 +0530

    original and created function

--- Module3/ES6/filter.js
+++ Module3/ES6/filter.js
@@ -1,8 +1,33 @@
 let a = [1,2,3,4];
 
-let filteredArray = a.filter(function(isEven) {
-    return isEven % 2 == 0;
-});
+function isEven(x) {
+    return x % 2 == 0;
+}
 
+let filteredArray = a.filter(isEven);
+
+console.log("Original filter function");
 console.log(a);
 console.log(filteredArray);
+
+
+// ------------------------------------
+
+// myFilter
+
+function myFilter(arr, f) {
+
+    let ans = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        if(f(arr[i])) {
+            ans.push(f(arr[i]));
+        }
+    }
+
+    return ans;
+}
+
+console.log("Created filter function");
+console.log(a);
+console.log(myFilter(a, isEven));
