8ce0df5007c1be04bdff8c7badf6a45dba203535
AnuragBisht71 <bishtanurag71@gmail.com>
2023-08-21 15:57:46 +0530

    destructuring object

---
+++ Module3/ES6/destructuringObj.js
@@ -0,0 +1,11 @@
+let address = {
+    city: "Noida",
+    region: "Gautam Buddh Nagar",
+    pin: 201301,
+}
+
+let { region, pin, city } = address;
+
+console.log(region);
+console.log(pin);
+console.log(city);
