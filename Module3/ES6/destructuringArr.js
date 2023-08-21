0e0b0dc6c47ed216890ec670ad794acd0b9b6c9a
AnuragBisht71 <bishtanurag71@gmail.com>
2023-08-21 16:07:35 +0530

    destructuring array

---
+++ Module3/ES6/destructuringArr.js
@@ -0,0 +1,8 @@
+let a = [1, 2, 3, 4, 5];
+
+let [firstVar, secondVar, , fourthVar] = a;
+// Empty space btw commas means skiping value
+
+console.log(firstVar);
+console.log(secondVar);
+console.log(fourthVar);
